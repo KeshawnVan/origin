7e62daaed57888b1aa774a3e6f5f5729dba409a9
Fankx <fankx@startimes.com.cn>
2017-11-08 13:49:27 +0800

    split

---
+++ originwebtest/src/test/java/TestF.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import star.utils.StringUtil;
+
+/**
+ * @author keshawn
+ * @date 2017/11/8
+ */
+public class TestF {
+    @Test
+    public void test(){
+        System.out.println(StringUtil.isNotEmpty("   "));
+    }
+}
