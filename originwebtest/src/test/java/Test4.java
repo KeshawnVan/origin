182dc087e2625df66839af00dc95a2b819a560fd
Fankx <fankx@startimes.com.cn>
2018-03-26 10:48:46 +0800

    sort

--- originwebtest/src/test/java/Test4.java
+++ originwebtest/src/test/java/Test4.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * @author keshawn
  * @date 2018/3/20
@@ -7,9 +9,28 @@ import org.junit.Test;
 public class Test4 {
 
     @Test
-    public void printThread(){
+    public void printThread() {
         System.out.println(Thread.currentThread().hashCode());
         System.out.println(Thread.currentThread().getId());
         System.out.println(Thread.currentThread().getName());
+    }
+
+    @Test
+    public void test() {
+        int k = 2;
+        Integer[] array = {2, 1, 4, 5, 9};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j <= k; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            if (i == k) {
+                System.out.println(array[k - 1]);
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
