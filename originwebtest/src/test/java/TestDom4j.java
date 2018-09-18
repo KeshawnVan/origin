e5d47f48bd167bb5f6252e0a7f2486c008f69932
Fankx <fankx@startimes.com.cn>
2018-09-18 16:59:38 +0800

    DOM4j and FileUtil

---
+++ originwebtest/src/test/java/TestDom4j.java
@@ -0,0 +1,37 @@
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.junit.Test;
+import star.utils.ClassUtil;
+
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+public class TestDom4j {
+
+    @Test
+    public void testParse() throws Exception{
+        SAXReader saxReader = new SAXReader();
+        Document document = saxReader.read(ClassUtil.getClassLoader().getResourceAsStream("students.xml"));
+        Element rootElement = document.getRootElement();
+        for (Iterator<Element> it = rootElement.elementIterator(); it.hasNext();) {
+            Element element = it.next();
+            parseElement(element);
+        }
+    }
+
+    private void parseElement(Element element) {
+        System.out.printf("< element %s start \r\n", element.getName());
+        System.out.printf("< element %s value is %s ", element.getName(), element.getText().trim());
+        for (Iterator<Attribute> ia = element.attributeIterator(); ia.hasNext();) {
+            Attribute attribute = ia.next();
+            System.out.printf("attribute is %s value is %s ", attribute.getName(), attribute.getValue());
+        }
+        for (Iterator<Element> ie = element.elementIterator(); ie.hasNext();) {
+            parseElement(ie.next());
+        }
+        System.out.printf(" element %s end >", element.getName());
+        System.out.println("");
+    }
+}
